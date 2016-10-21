43a1b8920af1f9ff44b8c866186b504218f63e15
kaili.wkl <kaili.wkl@alibaba-inc.com>
2016-10-21 16:42:14 +0800

    add new command

--- push/bin/push.js
+++ push/bin/push.js
@@ -3,22 +3,39 @@
 require('shelljs/global');
 var fs = require('fs');
 
+// 获取数据
 var argv = require('yargs')
   .option('m', {
     alias : 'msg',
     demand: false,
-    default: 'update',
+    default: (function(){
+      var argv2 = process.argv[2];
+      return argv2 && argv2.indexOf('-') != 0 ? argv2 : 'update';
+    })(),
     describe: '提交信息',
     type: 'string'
   })
-  .usage('Usage: push -m [options]')
+  .usage('Usage: push [-m] [options]')
   .example('push -m "update"')
+  .example('push "update"')
   .help('h')
   .alias('h', 'help')
   .argv;
-var branch = getBranch();
+var branch = (function(){
+  var b = '';
+  fs.readFile('.git/HEAD', 'utf8', function (err, data) {
+      if (err) {
+          echo('Error: Unable to read .git/HEAD', err);
+          exit(1);
+      } else {
+          b = data.split(': ').pop().trim().substring(11);
+      }
+  });
+  return b;
+})();
+var msg = argv.m || 'update';
 
-
+// 执行 git
 if (!which('git')) {
   echo('Sorry, this script requires git');
   exit(1);
@@ -29,7 +46,7 @@ if (exec('git add .').code !== 0) {
   exit(1);
 }
 echo('committing...');
-if (exec('git commit -am "' + argv.m + '"').code !== 0) {
+if (exec('git commit -am "' + msg + '"').code !== 0) {
   echo('Error: Git commit failed');
   exit(1);
 }
@@ -39,18 +56,3 @@ if (exec('git push origin ' + branch).code !== 0) {
   exit(1);
 }
 echo('=============== (;´▽`)y-~~ DONE! ===============');
-
-function getBranch(){
-	var branch = '';
-
-	fs.readFile('.git/HEAD', 'utf8', function (err, data) {
-	    if (err) {
-	        echo('Error: Unable to read .git/HEAD', err);
-	        exit(1);
-	    } else {
-	        branch = data.split(': ').pop().trim().substring(11);
-	    }
-	});
-
-	return branch;
-}
