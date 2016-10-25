ae650df354fb99119c77827aa26b50d34716f3a6
kaili.wkl <kaili.wkl@alibaba-inc.com>
2016-10-25 11:24:17 +0800

    fix file read bug

--- cmds/push.js
+++ cmds/push.js
@@ -21,35 +21,33 @@ Object.assign(exports, {
       .example('push "update"');
   },
   handler: (argv) => {
-    var branch = (function() {
-      var b = '';
-      fs.readFile('.git/HEAD', 'utf8', function(err, data) {
-        if (err) {
-          echo('Unable to read .git/HEAD', err);
-          exit(1);
-        } else {
-          b = data.split(': ').pop().trim().substring(11);
-        }
-      });
-      return b;
-    })();
     var msg = argv.msg || 'update';
+    var branch;
 
-    if (!which('git')) {
-      LETS.fail('Sorry, this script requires git');
-    }
-    echo('adding...');
-    if (exec('git add .').code !== 0) {
-      LETS.fail('Git add failed');
-    }
-    echo('committing...');
-    if (exec('git commit -m "' + msg + '"').code !== 0) {
-      LETS.fail('Git commit failed');
-    }
-    echo('pushing... git push origin ' + branch);
-    if (exec('git push origin ' + branch).code !== 0) {
-      LETS.fail('Git push failed');
-    }
-    LETS.done();
+    fs.readFile('.git/HEAD', 'utf8', function(err, data) {
+      if (err) {
+        echo('Unable to read .git/HEAD', err);
+        exit(1);
+      } else {
+        branch = data.split(': ').pop().trim().substring(11);
+        
+        if (!which('git')) {
+          LETS.fail('Sorry, this script requires git');
+        }
+        echo('adding...');
+        if (exec('git add .').code !== 0) {
+          LETS.fail('Git add failed');
+        }
+        echo('committing...');
+        if (exec('git commit -m "' + msg + '"').code !== 0) {
+          LETS.fail('Git commit failed');
+        }
+        echo('pushing... git push origin ' + branch);
+        if (exec('git push origin ' + branch).code !== 0) {
+          LETS.fail('Git push failed');
+        }
+        LETS.done();
+      }
+    });
   }
 });
